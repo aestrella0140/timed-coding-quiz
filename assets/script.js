99959adeaaa1c1ffabf9866a57bcaac578d0e6b1
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-07-12 18:48:21 -0700

    tried to add timer and if statements

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,63 @@
+
+var timerEl = document.querySelector(".timer");
+var startBtn = document.getElementsByClassName('next');
+
+var CurrentTime = 60;
+var timerId;
+
 document.getElementsByClassName('container')[0].style.display = "block";
 
 function next(id) {
-    document.getElementsByClassName('container')[id-1].style.display = "none";
+    document.getElementsByClassName('container')[id - 1].style.display = "none";
     document.getElementsByClassName('container')[id].style.display = "block";
 
 }
 
 function result() {
-    var score = 0;
+    var score = CurrentTime;
     if (document.getElementById('correct1').checked) {
         score++;
+    }else {
+        CurrentTime -5;
     }
     if (document.getElementById('correct2').checked) {
         score++;
+    }else {
+        CurrentTime -5;
     }
     if (document.getElementById('correct3').checked) {
         score++;
+    }else {
+        CurrentTime -5;
     }
     if (document.getElementById('correct4').checked) {
         score++;
+    }else {
+        CurrentTime -5;
     }
     if (document.getElementById('correct5').checked) {
         score++;
+    }else {
+        CurrentTime -5;
     }
     if (document.getElementById('correct6').checked) {
         score++;
+    }else {
+        CurrentTime -5;
     }
-    alert("your score is: "+ score)
+    alert("your score is: " + CurrentTime)
 }
 
-function countdown() {
-    var timeLeft = 60;
 
-    var timeInterval = setInterval(function () {
-        if(timeLeft > 1){
-            
-        }
-    })
+function countdown () {
+    CurrentTime--;
+    console.log(CurrentTime);
+    timerEl.textContent = CurrentTime;
 }
+
+function startTimer () {
+    startBtn.disabled = true;
+    timerId = setInterval(countdown, 1000);
+}
+
+startBtn.onclick = countdown;
